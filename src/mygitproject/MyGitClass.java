6b0637f424deb05684a1b500c23784cf5a5819e7
Islam D <43994821+IslamDuger@users.noreply.github.com>
2021-04-04 18:17:25 +0200

    Update MyGitClass.java

--- src/mygitproject/MyGitClass.java
+++ src/mygitproject/MyGitClass.java
@@ -5,13 +5,11 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Version 1");
-		
+		System.out.println("Version 1");		
 		System.out.println("version2");
 		System.out.println("version mesaji");
 		System.out.println("Uzak version mesaji");
-		int sayi=10;
-		
+		int sayi=10;		
 		System.out.println("yasin bey version mesaji");
 	}
 
